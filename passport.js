05715f7f791e4284fa806376b2d3ed5fc1ce8956
little-sunj <jsw5666@gmail.com>
2020-12-02 17:24:53 +0900

    ~6.3

--- passport.js
+++ passport.js
@@ -1,11 +1,11 @@
 import passport from "passport";
 import User from "./models/User";
 
-passport.use(User.createStragey());
-
 //serialization : which fields cookie will have. 
 //cookie should be small as possible
 //deserialization : get the cookie data and compare with the actual data
+
+passport.use(User.createStrategy());
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
