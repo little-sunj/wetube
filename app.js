54474b0784b2f6bbe2fde42bf1bc0f18b78a42f7
little-sunj <jsw5666@gmail.com>
2020-12-03 20:32:57 +0900

    6.5

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 import passport from "passport";
+import session from "express-session";
 import { localsMiddleware } from "./middlewares";
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
@@ -22,6 +23,11 @@ app.use("/static", express.static("static"));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
+app.use(session({
+    secret: process.env.COOKIE_SECRET,
+    resave: true,
+    saveUninitialized: false
+}));
 app.use(morgan("dev"));
 app.use(passport.initialize());
 app.use(passport.session());
